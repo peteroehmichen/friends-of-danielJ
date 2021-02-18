5cc333d27e304c5c45c0dba026a61ad987bb75a5
Peter Oehmichen <peter.oehmichen@mailbox.org>
2021-02-18 11:59:12 +0100

    linked redux to /app

--- client/src/start.js
+++ client/src/start.js
@@ -1,14 +1,29 @@
 // only render welcome page if the user specifically requests it. (is being sent there by server)
 
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import reduxPromise from "redux-promise";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
+
 import Welcome from "./welcome";
 import App from "./app";
+import reducer from "./reducer";
+
+const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(reduxPromise))
+);
 
 let elem;
 if (location.pathname === "/welcome") {
     elem = <Welcome />;
 } else {
-    elem = <App />;
+    elem = (
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
 }
 
 ReactDOM.render(elem, document.querySelector("main"));
